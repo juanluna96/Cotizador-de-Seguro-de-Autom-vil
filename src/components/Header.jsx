e0be89ebeb8ea15bc2f0528082433e7d49f14786
juanluna96 <juandluna17@gmail.com>
2021-02-15 17:52:47 -0500

    Creando la App y Ajustes

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = ({ titulo }) => {
+    return (
+        <div>
+            <header>
+                <h1>{ titulo }</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
